3dfa7314cb74037f615441ecc81362adaf0425aa
Mohamad Supangat <mohamad.supangat@clickoding.id>
2021-10-05 16:23:00 +0700

    download all treesitter maintained version

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,6 +5,7 @@
 -- Plugin: nvim-treesitter
 --- https://github.com/nvim-treesitter/nvim-treesitter
 require('nvim-treesitter.configs').setup {
+  ensure_installed = "maintained",
   highlight = {
     enable = true,
   },
