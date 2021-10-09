904ba77751341bdd37b896250f801f99403690fd
Mohamad Supangat <hantamkoding@gmail.com>
2021-10-09 19:05:53 +0700

    show git ignore in nvim tree lua

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -24,7 +24,7 @@ local g = vim.g
 
 --g.nvim_tree_width = 30
 g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
-g.nvim_tree_gitignore = 1
+g.nvim_tree_gitignore = 0
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_git_hl = 1
