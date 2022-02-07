9111a151739f485e22b972ec2fca0bf2f6f28e84
Mohamad Supangat <hantamkoding@gmail.com>
2022-02-07 18:37:26 +0700

    set indentline to default

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -3,9 +3,11 @@ vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
-    space_char_blankline = " ",
-    char = '▏',
+    -- space_char_blankline = "|",
+    -- char = '▏',
     show_current_context = false,
+    show_current_context_start = true,
+
     buftype_exclude = {"terminal", 'startify', 'nvim-tree', 'CHADTree', 'chad-tree', 'no-profile'},
     filetype_exclude = {"terminal", 'startify', 'nvim-tree', 'CHADTree', 'chad-tree', 'no-profile'},
     show_end_of_line = true,
