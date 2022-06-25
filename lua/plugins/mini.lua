1809cf5885c57b22a3c23227d0dae9b77d5125ea
Mohamad Supangat <hantamkoding@gmail.com>
2022-06-26 01:20:57 +0700

    indentscope chat

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -42,11 +42,11 @@ require('mini.indentscope').setup({
         -- Whether to first check input line to be a border of adjacent scope.
         -- Use it if you want to place cursor on function header to get scope of
         -- its body.
-        try_as_border = false
+        try_as_border = true
     },
 
     -- Which character to use for drawing scope indicator
-    symbol = '╎'
+    symbol = '▏'
 })
 
 local starter = require('mini.starter')
