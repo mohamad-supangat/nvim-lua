5d486811fd3c6a0cc08e8b63235776dbf3941877
Mohamad Supangat <hantamkoding@gmail.com>
2022-08-26 14:25:36 +0700

    pellate

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,27 +1,34 @@
--- local palette = require("mini.base16").mini_palette("#282828", "#fbf1c7", 100)
+local palette = require("mini.base16").mini_palette("#1e1e28", "#f5e0dc", 100)
+
 require("mini.base16").setup({
-    palette = {
-        base00 = '#1e1e28',
-        base01 = '#1a1826',
-        base02 = '#302d41',
-        base03 = '#575268',
-        base04 = '#6e6c7c',
-        base05 = '#d7dae0',
-        base06 = '#f5e0dc',
-        base07 = '#c9cbff',
-        base08 = '#f28fad',
-        base09 = '#f8bd96',
-        base0A = '#fae3b0',
-        base0B = '#abe9b3',
-        base0C = '#b5e8e0',
-        base0D = '#96cdfb',
-        base0E = '#ddb6f2',
-        base0F = '#f2cdcd',
-    },
+    palette = palette,
     name = "minischeme",
     use_cterm = true
 })
---
+
+-- require("mini.base16").setup({
+--     palette = {
+--         base00 = '#1e1e28',
+--         base01 = '#1a1826',
+--         base02 = '#302d41',
+--         base03 = '#575268',
+--         base04 = '#6e6c7c',
+--         base05 = '#d7dae0',
+--         base06 = '#f5e0dc',
+--         base07 = '#c9cbff',
+--         base08 = '#f28fad',
+--         base09 = '#f8bd96',
+--         base0A = '#fae3b0',
+--         base0B = '#abe9b3',
+--         base0C = '#b5e8e0',
+--         base0D = '#96cdfb',
+--         base0E = '#ddb6f2',
+--         base0F = '#f2cdcd',
+--     },
+--     name = "minischeme",
+--     use_cterm = true
+-- })
+
 -- require('mini.indentscope').setup({
 --     draw = {
 --         -- Delay (in ms) between event and start of drawing scope indicator
