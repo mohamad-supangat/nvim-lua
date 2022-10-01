6d757a23b522ac562df18e116e33af069cdecd8a
Mohamad Supangat <hantamkoding@gmail.com>
2022-10-01 15:36:22 +0700

    add blade as auto close tag

--- lua/plugins/auto-closetag.lua
+++ lua/plugins/auto-closetag.lua
@@ -1,5 +1,5 @@
 local g = vim.g -- global variables
 
-g.closetag_filenames = '*.html,*.xhtml,*.phtml,*.vue,*.xml'
+g.closetag_filenames = '*.html,*.xhtml,*.phtml,*.vue,*.xml,*.blade.php'
 g.closetag_xhtml_filenames = '*.xhtml,*.jsx,*.vue,*.xml'
-g.closetag_filetypes = 'html,xhtml,phtml,vue,xml'
+g.closetag_filetypes = 'html,xhtml,phtml,vue,xml,blade'
