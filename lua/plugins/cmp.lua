7237bf17ee271208b1ccf5521bc73f3b0668425b
Mohamad Supangat <hantamkoding@gmail.com>
2022-10-03 11:22:05 +0700

    comment snipt

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,7 +5,7 @@ local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then return end
 
 require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({paths = {"./snippets"}})
+-- require("luasnip.loaders.from_vscode").lazy_load({paths = {"./snippets"}})
 
 local check_backspace = function()
     local col = vim.fn.col(".") - 1
