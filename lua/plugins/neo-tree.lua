ec8c1f932ff06de40c756849aa10ed3b1a2d5bbb
Mohamad Supangat <hantamkoding@gmail.com>
2022-10-03 13:43:19 +0700

    add status_check in neo-rtree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,9 +1,12 @@
+local status_ok, neo_tree = pcall(require, "neo-tree")
+if not status_ok then return end
+
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
 map("n", "<C-n>", ":NeoTreeRevealToggle<CR>", default_opts)
 
-require("neo-tree").setup({
+neo_tree.setup({
     filesystem = {
         filtered_items = {hide_dotfiles = false, hide_gitignored = false},
         follow_current_file = true,
