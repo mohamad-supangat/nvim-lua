66e2b267d122595274adad06fb408975b8145b4f
Mohamad Supangat <hantamkoding@gmail.com>
2022-10-05 14:26:46 +0700

    auto setup handler mason_lsp

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -15,8 +15,23 @@ mason.setup({
 --  │ mason lsp config helper                                  │
 --  ╰──────────────────────────────────────────────────────────╯
 local lsp_status_ok, mason_lsp = pcall(require, "mason-lspconfig")
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+
 if not lsp_status_ok then return end
 mason_lsp.setup({
-    ensure_installed = {'sumneko_lua'},
+    ensure_installed = { 'sumneko_lua' },
 })
 
+
+mason_lsp.setup_handlers({
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+    ["sumneko_lua"] = function()
+        lspconfig.sumneko_lua.setup(require('lsp.settings.sumneko_lua'))
+    end,
+
+    ["pyright"] = function()
+        lspconfig.pyright.setup(require('lsp.settings.pyright'))
+    end,
+})
