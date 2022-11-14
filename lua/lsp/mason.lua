2a41c660fda6f5e9a45b561e40ae23ec7728aff0
Mohamad Supangat <hantamkoding@gmail.com>
2022-11-14 10:21:43 +0700

    remind

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -1,14 +1,16 @@
 local status_ok, mason = pcall(require, "mason")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
 mason.setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 --  ╭──────────────────────────────────────────────────────────╮
@@ -17,26 +19,28 @@ mason.setup({
 local lsp_status_ok, mason_lsp = pcall(require, "mason-lspconfig")
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 
-if not lsp_status_ok then return end
-mason_lsp.setup({ ensure_installed = { 'sumneko_lua' } })
+if not lsp_status_ok then
+	return
+end
+mason_lsp.setup({ ensure_installed = { "sumneko_lua" } })
 
 mason_lsp.setup_handlers({
-    function(server) -- default handler (optional)
-        opts = {
-            on_attach = require("lsp.handlers").on_attach,
-            capabilities = require("lsp.handlers").capabilities
-        }
+	function(server) -- default handler (optional)
+		opts = {
+			on_attach = require("lsp.handlers").on_attach,
+			capabilities = require("lsp.handlers").capabilities,
+		}
 
-        if server == "sumneko_lua" then
-            local sumneko_opts = require "lsp.settings.sumneko_lua"
-            opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-        end
+		if server == "sumneko_lua" then
+			local sumneko_opts = require("lsp.settings.sumneko_lua")
+			opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+		end
 
-        if server == "pyright" then
-            local pyright_opts = require "lsp.settings.pyright"
-            opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-        end
+		if server == "pyright" then
+			local pyright_opts = require("lsp.settings.pyright")
+			opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+		end
 
-        lspconfig[server].setup(opts)
-    end
+		lspconfig[server].setup(opts)
+	end,
 })
