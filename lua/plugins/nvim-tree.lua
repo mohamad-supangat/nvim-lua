2635c608881ec20ef37a5f630feaa7ba2397a9b9
Mohamad Supangat <hantamkoding@gmail.com>
2022-12-24 10:02:33 +0700

    format using nvim lsp

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,9 +1,10 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
 local map = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
-
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -11,36 +12,36 @@ vim.g.loaded_netrwPlugin = 1
 map("n", "<C-n>", ":NvimTreeToggle<CR>", default_opts)
 
 nvim_tree.setup({
-  sort_by             = "case_sensitive",
-  git                 = {
-    ignore = false,
-  },
-  actions             = {
-    open_file = {
-      quit_on_open = true
-    }
-  },
-  view                = {
-    preserve_window_proportions = true,
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "l", action = "edit" },
-        { key = "h", action = "close_node" },
-        -- { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer            = {
-    group_empty = true,
-  },
-  filters             = {
-    dotfiles = false,
-  },
-  live_filter         = {
-    always_show_folders = false
-  },
-  update_focused_file = {
-    enable = true
-  }
+	sort_by = "case_sensitive",
+	git = {
+		ignore = false,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
+	view = {
+		preserve_window_proportions = true,
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "l", action = "edit" },
+				{ key = "h", action = "close_node" },
+				-- { key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	live_filter = {
+		always_show_folders = false,
+	},
+	update_focused_file = {
+		enable = true,
+	},
 })
