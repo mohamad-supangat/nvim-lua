7a4fb9f689f053105d472747d95571ba07ec4663
Mohamad Supangat <hantamkoding@gmail.com>
2023-01-17 20:12:54 +0700

    disable mini sesson because unused

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -88,7 +88,7 @@ starter.setup({
 	evaluate_single = true,
 	items = {
 		my_items,
-		starter.sections.sessions(5, true), -- starter.sections.telescope(),
+		-- starter.sections.sessions(5, true), -- starter.sections.telescope(),
 		starter.sections.builtin_actions(),
 		starter.sections.recent_files(10, false),
 		starter.sections.recent_files(10, true),
@@ -209,25 +209,25 @@ require("mini.surround").setup({
 
 require("mini.bufremove").setup({ set_vim_settings = true })
 
-require("mini.sessions").setup({
-	-- Whether to read latest session if Neovim opened without file arguments
-	autoread = false,
-	-- Whether to write current session before quitting Neovim
-	autowrite = true,
-	-- Directory where global sessions are stored (use `''` to disable)
-	-- directory = --<"session" subdir of user data directory from |stdpath()|>,
-
-	-- File for local session (use `''` to disable)
-	file = "",
-	-- Whether to force possibly harmful actions (meaning depends on function)
-	force = { read = false, write = true, delete = false },
-	-- Hook functions for actions. Default `nil` means 'do nothing'.
-	hooks = {
-		-- Before successful action
-		pre = { read = nil, write = nil, delete = nil },
-		-- After successful action
-		post = { read = nil, write = nil, delete = nil },
-	},
-	-- Whether to print session path after action
-	verbose = { read = false, write = true, delete = true },
-})
+-- require("mini.sessions").setup({
+-- 	-- Whether to read latest session if Neovim opened without file arguments
+-- 	autoread = false,
+-- 	-- Whether to write current session before quitting Neovim
+-- 	autowrite = true,
+-- 	-- Directory where global sessions are stored (use `''` to disable)
+-- 	-- directory = --<"session" subdir of user data directory from |stdpath()|>,
+--
+-- 	-- File for local session (use `''` to disable)
+-- 	file = "",
+-- 	-- Whether to force possibly harmful actions (meaning depends on function)
+-- 	force = { read = false, write = true, delete = false },
+-- 	-- Hook functions for actions. Default `nil` means 'do nothing'.
+-- 	hooks = {
+-- 		-- Before successful action
+-- 		pre = { read = nil, write = nil, delete = nil },
+-- 		-- After successful action
+-- 		post = { read = nil, write = nil, delete = nil },
+-- 	},
+-- 	-- Whether to print session path after action
+-- 	verbose = { read = false, write = true, delete = true },
+-- })
