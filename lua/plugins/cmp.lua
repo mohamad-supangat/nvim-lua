176d827d3858fee53c932c52ced6ddc2eb5852a5
Mohamad Supangat <hantamkoding@gmail.com>
2023-01-30 08:37:17 +0700

    disable tab key in nvim cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -84,26 +84,26 @@ cmp.setup({
 		-- Accept currently selected item. If none selected, `select` first item.
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+		-- ["<Tab>"] = cmp.mapping(function(fallback)
+		-- 	if cmp.visible() then
+		-- 		cmp.select_next_item()
+		-- 	elseif luasnip.expand_or_jumpable() then
+		-- 		luasnip.expand_or_jump()
+		-- 	elseif has_words_before() then
+		-- 		cmp.complete()
+		-- 	else
+		-- 		fallback()
+		-- 	end
+		-- end, { "i", "s" }),
+		-- ["<S-Tab>"] = cmp.mapping(function(fallback)
+		-- 	if cmp.visible() then
+		-- 		cmp.select_prev_item()
+		-- 	elseif luasnip.jumpable(-1) then
+		-- 		luasnip.jump(-1)
+		-- 	else
+		-- 		fallback()
+		-- 	end
+		-- end, { "i", "s" }),
 	}),
 	formatting = {
 		format = function(entry, vim_item)
@@ -128,11 +128,11 @@ cmp.setup({
 		{ name = "luasnip" },
 		{
 			name = "buffer",
-			option = {
-				get_bufnrs = function()
-					return vim.api.nvim_list_bufs()
-				end,
-			},
+			-- option = {
+			-- 	get_bufnrs = function()
+			-- 		return vim.api.nvim_list_bufs()
+			-- 	end,
+			-- },
 		},
 		{ name = "nvim_lsp_signature_help" },
 		{ name = "path" },
@@ -168,50 +168,3 @@ cmp.setup.cmdline(":", {
 		{ name = "cmdline" },
 	}),
 })
-
--- local cmp_h = {
--- 	PmenuSel = { bg = "#FF0000", fg = "NONE" },
--- 	Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
---
--- 	CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", fmt = "strikethrough" },
--- 	CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE", fmt = "bold" },
--- 	CmpItemAbbrMatchFuzzy = { fg = "#82AAFF", bg = "NONE", fmt = "bold" },
--- 	CmpItemMenu = { fg = "#C792EA", bg = "NONE", fmt = "italic" },
---
--- 	CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
--- 	CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
--- 	CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
---
--- 	CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
--- 	CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
--- 	CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
---
--- 	CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
--- 	CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
--- 	CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
---
--- 	CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
--- 	CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
--- 	CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
--- 	CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
--- 	CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
---
--- 	CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
--- 	CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
---
--- 	CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
--- 	CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
--- 	CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
---
--- 	CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
--- 	CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
--- 	CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
---
--- 	CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
--- 	CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
--- 	CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
--- }
---
--- for key, value in pairs(cmp_h) do
--- 	vim.api.nvim_set_hl(0, key, { fg = value.fg, bg = value.bg })
--- end
