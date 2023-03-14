eb45bcf037d2234427e042c0d0a2fd0862792002
Mohamad Supangat <hantamkoding@gmail.com>
2023-03-14 10:07:17 +0700

    remove tab line and using incline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -115,16 +115,18 @@ require("mini.comment").setup({
 -- vim.cmd([[hi WinSeparator guibg=none]])
 require("mini.statusline").setup({ set_vim_settings = false })
 
-require("mini.tabline").setup({
-	-- Whether to show file icons (requires 'kyazdani42/nvim-web-devicons')
-	show_icons = true,
-	-- Whether to set Vim's settings for tabline (make it always shown and
-	-- allow hidden buffers)
-	set_vim_settings = true,
-	-- Where to show tabpage section in case of multiple vim tabpages.
-	-- One of 'left', 'right', 'none'.
-	tabpage_section = "right",
-})
+-- require("mini.tabline").setup({
+-- 	-- Whether to show file icons (requires 'kyazdani42/nvim-web-devicons')
+-- 	show_icons = true,
+-- 	-- Whether to set Vim's settings for tabline (make it always shown and
+-- 	-- allow hidden buffers)
+-- 	set_vim_settings = true,
+-- 	-- Where to show tabpage section in case of multiple vim tabpages.
+-- 	-- One of 'left', 'right', 'none'.
+-- 	tabpage_section = "right",
+-- })
+
+
 --
 -- require('mini.pairs').setup({
 --     -- In which modes mappings from this `config` should be created
--- lua/plugins/vim-closetag.lua
+++ lua/plugins/vim-closetag.lua
@@ -1,5 +1,4 @@
-vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml"
+vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml,*.xml"
 vim.g.closetag_xhtml_filenames = "*.xhtml,*.jsx,*.xml"
-vim.g.closetag_filetypes = "html,xhtml,phtml"
-vim.g.closetag_xhtml_filetypes = 'xhtml,jsx,xml'
-
+vim.g.closetag_filetypes = "html,xhtml,phtml,xml"
+vim.g.closetag_xhtml_filetypes = "xhtml,jsx,xml"
