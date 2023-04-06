845cb33748cb1daed5ea1a7c6ff66316720d3f0b
Mohamad Supangat <hantamkoding@gmail.com>
2023-04-06 20:39:45 +0700

    duplicate close ta

--- lua/plugins/vim-closetag.lua
+++
@@ -1,4 +0,0 @@
-vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml,*.xml"
-vim.g.closetag_xhtml_filenames = "*.xhtml,*.jsx,*.xml"
-vim.g.closetag_filetypes = "html,xhtml,phtml,xml"
-vim.g.closetag_xhtml_filetypes = "xhtml,jsx,xml"
