a7b40602b9f905e7da22b3b189aff15324d990d8
Mohamad Supangat <hantamkoding@gmail.com>
2023-07-10 08:54:17 +0700

    fix: emmet

--- lua/pre-settings.lua
+++ lua/pre-settings.lua
@@ -2,4 +2,4 @@ local variables = require("variables")
 vim.g.polyglot_disabled = variables.filetypes
 
 require("plugins.vim-closetag")
-require("plugins.vim-emmet")
+-- require("plugins.vim-emmet")
