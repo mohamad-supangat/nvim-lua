1e703027520458bf09286141f497b191e0d2c747
Mohamad Supangat <hantamkoding@gmail.com>
2023-09-14 10:24:09 +0700

    update catpucin using config from https://github.com/ayamir/nvimdots/blob/main/lua/modules/configs/ui/catppuccin.lua

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,25 +3,44 @@ if not status_ok then
     return
 end
 
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+
+
+local transparent_background = true
+local clear = {}
+-- vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
 catppuccin.setup({
-    transparent_background = false,
+    flavour = "mocha",
+    background = { light = "latte", dark = "mocha" },
+    show_end_of_buffer = true,
+    transparent_background = transparent_background,
     term_colors = true,
     dim_inactive = { enabled = false, shade = "dark", percentage = 0.15 },
     styles = {
+        -- comments = { "italic" },
+        -- conditionals = { "italic" },
+        -- loops = {},
+        -- functions = { "italic" },
+        -- keywords = {},
+        -- strings = {},
+        -- variables = { "italic" },
+        -- numbers = { "bold" },
+        -- booleans = {},
+        -- properties = {},
+        -- types = { "bold" },
+        -- operators = {},
         comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = { "italic" },
-        keywords = {},
+        properties = { "italic" },
+        functions = { "bold" },
+        keywords = { "italic" },
+        operators = { "bold" },
+        conditionals = { "bold" },
+        loops = { "bold" },
+        booleans = { "bold", "italic" },
+        numbers = {},
+        types = {},
         strings = {},
-        variables = { "italic" },
-        numbers = { "bold" },
-        booleans = {},
-        properties = {},
-        types = { "bold" },
-        operators = {},
+        variables = {},
     },
     integrations = {
         telescope = {
@@ -57,13 +76,91 @@ catppuccin.setup({
         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
     },
     color_overrides = {
-        mocha = {
-            base = "#000000",
-            mantle = "#000000",
-            crust = "#000000",
-        },
+        -- mocha = {
+        --     base = "#000000",
+        --     mantle = "#000000",
+        --     crust = "#000000",
+        -- },
     },
-    highlight_overrides = {},
+    highlight_overrides = {
+        ---@param cp palette
+        all = function(cp)
+            return {
+                -- For base configs
+                NormalFloat = { fg = cp.text, bg = transparent_background and cp.none or cp.mantle },
+                FloatBorder = {
+                    fg = transparent_background and cp.blue or cp.mantle,
+                    bg = transparent_background and cp.none or cp.mantle,
+                },
+                CursorLineNr = { fg = cp.green },
+
+                -- For native lsp configs
+                DiagnosticVirtualTextError = { bg = cp.none },
+                DiagnosticVirtualTextWarn = { bg = cp.none },
+                DiagnosticVirtualTextInfo = { bg = cp.none },
+                DiagnosticVirtualTextHint = { bg = cp.none },
+                LspInfoBorder = { link = "FloatBorder" },
+
+                -- For mason.nvim
+                MasonNormal = { link = "NormalFloat" },
+
+                -- For indent-blankline
+                IndentBlanklineChar = { fg = cp.surface0 },
+                IndentBlanklineContextChar = { fg = cp.surface2, style = { "bold" } },
+
+                -- For nvim-cmp and wilder.nvim
+                Pmenu = { fg = cp.overlay2, bg = transparent_background and cp.none or cp.base },
+                PmenuBorder = { fg = cp.surface1, bg = transparent_background and cp.none or cp.base },
+                PmenuSel = { bg = cp.green, fg = cp.base },
+                CmpItemAbbr = { fg = cp.overlay2 },
+                CmpItemAbbrMatch = { fg = cp.blue, style = { "bold" } },
+                CmpDoc = { link = "NormalFloat" },
+                CmpDocBorder = {
+                    fg = transparent_background and cp.surface1 or cp.mantle,
+                    bg = transparent_background and cp.none or cp.mantle,
+                },
+
+                -- For fidget
+                FidgetTask = { bg = cp.none, fg = cp.surface2 },
+                FidgetTitle = { fg = cp.blue, style = { "bold" } },
+
+                -- For nvim-tree
+                NvimTreeRootFolder = { fg = cp.pink },
+                NvimTreeIndentMarker = { fg = cp.surface2 },
+
+                -- For trouble.nvim
+                TroubleNormal = { bg = transparent_background and cp.none or cp.base },
+
+                -- For telescope.nvim
+                TelescopeMatching = { fg = cp.lavender },
+                TelescopeResultsDiffAdd = { fg = cp.green },
+                TelescopeResultsDiffChange = { fg = cp.yellow },
+                TelescopeResultsDiffDelete = { fg = cp.red },
+
+                -- For glance.nvim
+                GlanceWinBarFilename = { fg = cp.subtext1, style = { "bold" } },
+                GlanceWinBarFilepath = { fg = cp.subtext0, style = { "italic" } },
+                GlanceWinBarTitle = { fg = cp.teal, style = { "bold" } },
+                GlanceListCount = { fg = cp.lavender },
+                GlanceListFilepath = { link = "Comment" },
+                GlanceListFilename = { fg = cp.blue },
+                GlanceListMatch = { fg = cp.lavender, style = { "bold" } },
+                GlanceFoldIcon = { fg = cp.green },
+
+                -- For nvim-treehopper
+                TSNodeKey = {
+                    fg = cp.peach,
+                    bg = transparent_background and cp.none or cp.base,
+                    style = { "bold", "underline" },
+                },
+
+                -- For treesitter
+                ["@keyword.return"] = { fg = cp.pink, style = clear },
+                ["@error.c"] = { fg = cp.none, style = clear },
+                ["@error.cpp"] = { fg = cp.none, style = clear },
+            }
+        end,
+    },
 })
 
 vim.cmd([[colorscheme catppuccin]])
