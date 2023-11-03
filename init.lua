b410679314508eb37350b5e2eba85afcd4471834
Mohamad Supangat <hantamkoding@gmail.com>
2023-11-03 08:48:44 +0700

    add quasar snippets

--- init.lua
+++ init.lua
@@ -71,11 +71,18 @@ require("lazy").setup({
 
         "L3MON4D3/LuaSnip",
         "rafamadriz/friendly-snippets",
+
+        -- odoo snippets
         "droggol/VscOdooSnippets",
+
+        -- laravel snippets
         "onecentlin/laravel5-snippets-vscode",
         "onecentlin/laravel-blade-snippets-vscode",
         "ahinkle/vscode-laravel-model-snippets",
         "use-the-fork/laravel-vscode-snippets",
+
+        -- quasar snippets
+        "Abdelaziz18003/vscode-quasar-snippets",
         "mohamad-supangat/snippets",
     },
 
