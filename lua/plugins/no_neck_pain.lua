e58f0544a33a6fd74e40902ed100594e7f1ebb31
Mohamad Supangat <hantamkoding@gmail.com>
2023-12-14 11:04:21 +0700

    add: no neck pain plugin

---
+++ lua/plugins/no_neck_pain.lua
@@ -0,0 +1,11 @@
+return {
+    "shortcuts/no-neck-pain.nvim",
+    opts = {
+        autocmds = {
+            enableOnVimEnter = true
+        },
+        mappings = {
+            enabled = true
+        }
+    }
+}
