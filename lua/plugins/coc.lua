6785d0561f6a2a139ae42dc0685ffc69fddf26ff
Mohamad Supangat <hantamkoding@gmail.com>
2023-12-29 14:55:36 +0700

    fix: coc enable and disable common setting

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -8,60 +8,72 @@ local default_opts = { noremap = true, silent = true }
 
 
 -- coc multiple cursor
-vim.api.nvim_set_keymap('n', "<C-c>", "<Plug>(coc-cursors-position)", default_opts)
-vim.api.nvim_set_keymap('n', "<C-d>", "<Plug>(coc-cursors-word)", default_opts)
-vim.api.nvim_set_keymap('x', "<C-d>", "<Plug>(coc-cursors-range)", default_opts)
-vim.api.nvim_set_keymap('n', "<leader>x", "<Plug>(coc-cursors-operator)", default_opts)
+map('n', "<C-c>", "<Plug>(coc-cursors-position)", default_opts)
+map('n', "<C-d>", "<Plug>(coc-cursors-word)", default_opts)
+map('x', "<C-d>", "<Plug>(coc-cursors-range)", default_opts)
+map('n', "<leader>x", "<Plug>(coc-cursors-operator)", default_opts)
 
 
 -- coc-explorer
-vim.api.nvim_set_keymap("n", "<C-n>", ":CocCommand explorer<CR>", default_opts) -- open/close
+map("n", "<C-n>", ":CocCommand explorer<CR>", default_opts) -- open/close
 
 
-vim.api.nvim_set_keymap("n", "<leader>.", "<Plug>(coc-codeaction)", {})
-vim.api.nvim_set_keymap("n", "<leader>l", ":CocCommand eslint.executeAutofix<CR>", {})
-vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", { silent = true })
-vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<CR>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
+map("n", "<leader>.", "<Plug>(coc-codeaction)", {})
+map("n", "<leader>l", ":CocCommand eslint.executeAutofix<CR>", {})
+map("n", "gd", "<Plug>(coc-definition)", { silent = true })
+map("n", "K", ":call CocActionAsync('doHover')<CR>", { silent = true, noremap = true })
+map("n", "<leader>rn", "<Plug>(coc-rename)", {})
 
 
 -- snippets
 -- map("i", "<leader>s", "<Plug>(coc-snippets-expand)", default_opts)
-vim.api.nvim_set_keymap("x", "<leader>x", "<Plug>(coc-convert-snippet)", default_opts)
+map("x", "<leader>x", "<Plug>(coc-convert-snippet)", default_opts)
 
--- vim.api.nvim_set_keymap("v", "<leader>ss", "<Plug>(coc-snippets-select)", default_opts)
+-- map("v", "<leader>ss", "<Plug>(coc-snippets-select)", default_opts)
 
 -- formater command
-vim.api.nvim_set_keymap("n", "<leader>f", ":call CocActionAsync('format')<CR>", { noremap = true })
-vim.api.nvim_set_keymap("x", "<leader>f", " <Plug>(coc-format-selected)<CR>", { noremap = true })
-vim.api.nvim_set_keymap("v", "<leader>f", " <Plug>(coc-format-selected)<CR>", { noremap = true })
+map("n", "<leader>f", ":call CocActionAsync('format')<CR>", { noremap = true })
+map("x", "<leader>f", " <Plug>(coc-format-selected)<CR>", { noremap = true })
+map("v", "<leader>f", " <Plug>(coc-format-selected)<CR>", { noremap = true })
+
+
+-- diagnostic
+
+map("n", "[g", "<Plug>(coc-diagnostic-prev)", { noremap = true })
+map("n", "]g", "<Plug>(coc-diagnostic-next)", { noremap = true })
+map("n", "<leader>xx", ":CocList diagnostic", { noremap = true })
+map("n", "gl", ":call CocActionAsync('diagnosticInfo')<CR>", default_opts)
+
+
+
+
 
 
 
 -- show list in fzf
-vim.api.nvim_set_keymap("n", "<leader>coc", ":CocList<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>P", ":CocList commands<CR>", { noremap = true })
+map("n", "<leader>coc", ":CocList<CR>", { noremap = true })
+map("n", "<leader>P", ":CocList commands<CR>", { noremap = true })
 
 
-vim.api.nvim_set_keymap("i", "<C-space>", "coc#refresh()", { silent = true, expr = true })
+map("i", "<C-space>", "coc#refresh()", { silent = true, expr = true })
 
-vim.api.nvim_set_keymap("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'",
+map("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'",
     { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'",
+map("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'",
     { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<C-j>", "coc#pum#visible() ? coc#pum#next(1) : '<C-j>'",
+map("i", "<C-j>", "coc#pum#visible() ? coc#pum#next(1) : '<C-j>'",
     { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<C-k>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-k>'",
-    { noremap = true, silent = true, expr = true })
-
-
-vim.api.nvim_set_keymap("i", "<down>", "coc#pum#visible() ? coc#pum#next(1) : '<down>'",
-    { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<up>", "coc#pum#visible() ? coc#pum#prev(1) : '<up>'",
+map("i", "<C-k>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-k>'",
     { noremap = true, silent = true, expr = true })
 
 
-vim.api.nvim_set_keymap("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
+map("i", "<down>", "coc#pum#visible() ? coc#pum#next(1) : '<down>'",
+    { noremap = true, silent = true, expr = true })
+map("i", "<up>", "coc#pum#visible() ? coc#pum#prev(1) : '<up>'",
+    { noremap = true, silent = true, expr = true })
+
+
+map("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
     { silent = true, expr = true, noremap = true })
 
 
