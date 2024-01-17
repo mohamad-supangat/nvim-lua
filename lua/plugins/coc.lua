683e9a7b1bb38ff13823e349f786b2d82c425f41
Mohamad Supangat <hantamkoding@gmail.com>
2024-01-17 09:38:51 +0700

    coc.lua fix mapping

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -2,72 +2,7 @@
 -- Neovim API aliases
 -----------------------------------------------------------
 --local map = map  -- set global keymap
-local g = vim.g -- global variables
-local map = vim.api.nvim_set_keymap
-local default_opts = { noremap = true, silent = true }
-
-
--- coc multiple cursor
-map('n', "<leader>c", "<Plug>(coc-cursors-position)", default_opts)
-map('n', "<leader>d", "<Plug>(coc-cursors-word)", default_opts)
-map('x', "<leader>d", "<Plug>(coc-cursors-range)", default_opts)
-map('n', "<leader>x", "<Plug>(coc-cursors-operator)", default_opts)
-
-
--- coc-explorer
-map("n", "<C-n>", ":CocCommand explorer<CR>", default_opts) -- open/close
-
-
-map("n", "<leader>.", "<Plug>(coc-codeaction)", {})
-map("n", "<leader>l", ":CocCommand eslint.executeAutofix<CR>", {})
-map("n", "gd", "<Plug>(coc-definition)", { silent = true })
-map("n", "K", ":call CocActionAsync('doHover')<CR>", { silent = true, noremap = true })
-map("n", "<leader>rn", "<Plug>(coc-rename)", {})
-
-
--- snippets
--- map("i", "<leader>s", "<Plug>(coc-snippets-expand)", default_opts)
-map("x", "<leader>x", "<Plug>(coc-convert-snippet)", default_opts)
--- map("v", "<leader>ss", "<Plug>(coc-snippets-select)", default_opts)
-
--- formater command
-map("n", "<leader>f", ":call CocActionAsync('format')<CR>", { noremap = true })
-map("x", "<leader>f", " <Plug>(coc-format-selected)<CR>", { noremap = true })
-map("v", "<leader>f", " <Plug>(coc-format-selected)<CR>", { noremap = true })
-
-
--- diagnostic
-map("n", "[g", "<Plug>(coc-diagnostic-prev)", { noremap = true })
-map("n", "]g", "<Plug>(coc-diagnostic-next)", { noremap = true })
-map("n", "<leader>xx", "<cmd>CocDiagnostic<cr>", { noremap = true })
-map("n", "gl", ":call CocActionAsync('diagnosticInfo')<CR>", default_opts)
-
--- show list in fzf
-map("n", "<leader>coc", ":CocList<CR>", { noremap = true })
-map("n", "<leader>P", ":CocList commands<CR>", { noremap = true })
-
-
-map("i", "<C-space>", "coc#refresh()", { silent = true, expr = true })
-
-map("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'",
-    { noremap = true, silent = true, expr = true })
-map("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'",
-    { noremap = true, silent = true, expr = true })
-map("i", "<C-j>", "coc#pum#visible() ? coc#pum#next(1) : '<C-j>'",
-    { noremap = true, silent = true, expr = true })
-map("i", "<C-k>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-k>'",
-    { noremap = true, silent = true, expr = true })
-map("i", "<down>", "coc#pum#visible() ? coc#pum#next(1) : '<down>'",
-    { noremap = true, silent = true, expr = true })
-map("i", "<up>", "coc#pum#visible() ? coc#pum#prev(1) : '<up>'",
-    { noremap = true, silent = true, expr = true })
-map("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
-    { silent = true, expr = true, noremap = true })
-
-
-map("n", "<f7>", "<cmd>CocOutline<cr>", default_opts)
-
-g.coc_global_extensions = { 'coc-json', 'coc-marketplace', 'coc-snippets', 'coc-lua', 'coc-prettier', 'coc-explorer',
+vim.g.coc_global_extensions = { 'coc-json', 'coc-marketplace', 'coc-snippets', 'coc-lua', 'coc-prettier', 'coc-explorer',
     'coc-pairs', 'coc-emmet' }
 
 return {
@@ -75,5 +10,113 @@ return {
     branch = 'release',
     dependencies = {
         "nvim-tree/nvim-web-devicons"
+    },
+    keys = {
+        -- coc multiple cursor
+        { mode = 'n', "<leader>c",   "<Plug>(coc-cursors-position)" },
+        { mode = 'n', "<leader>d",   "<Plug>(coc-cursors-word)" },
+        { mode = 'x', "<leader>d",   "<Plug>(coc-cursors-range)" },
+        { mode = 'n', "<leader>x",   "<Plug>(coc-cursors-operator)" },
+
+
+        -- coc-explorer
+        { mode = "n", "<C-n>",       ":CocCommand explorer<CR>" },
+
+
+        { mode = "n", "<leader>.",   "<Plug>(coc-codeaction)" },
+        { mode = "n", "<leader>l",   ":CocCommand eslint.executeAutofix<CR>" },
+        { mode = "n", "gd",          "<Plug>(coc-definition)",                    silent = true },
+        { mode = "n", "K",           ":call CocActionAsync('doHover')<CR>",       silent = true, noremap = true },
+        { mode = "n", "<leader>rn",  "<Plug>(coc-rename)" },
+
+
+        -- snippets
+        -- { mode = "i", "<leader>s", "<Plug>(coc-snippets-expand)"},
+        { mode = "x", "<leader>x",   "<Plug>(coc-convert-snippet)" },
+        -- { mode = "v", "<leader>ss", "<Plug>(coc-snippets-select)"},
+
+        -- formater command
+        { mode = "n", "<leader>f",   ":call CocActionAsync('format')<CR>" },
+        { mode = "x", "<leader>f",   " <Plug>(coc-format-selected)<CR>" },
+        { mode = "v", "<leader>f",   " <Plug>(coc-format-selected)<CR>" },
+
+
+        -- diagnostic
+        { mode = "n", "[g",          "<Plug>(coc-diagnostic-prev)" },
+        { mode = "n", "]g",          "<Plug>(coc-diagnostic-next)" },
+        { mode = "n", "<leader>xx",  "<cmd>CocDiagnostic<cr>" },
+        { mode = "n", "gl",          ":call CocActionAsync('diagnosticInfo')<CR>" },
+
+        -- show list in fzf
+        { mode = "n", "<leader>coc", ":CocList<CR>" },
+        { mode = "n", "<leader>P",   ":CocList commands<CR>" },
+
+
+        { mode = "i", "<C-space>",   "coc#refresh()",                             silent = true, expr = true },
+
+        {
+            mode = "i",
+            "<TAB>",
+            "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'",
+            noremap = true,
+            silent = true,
+            expr = true
+        },
+
+        {
+            mode = "i",
+            "<S-TAB>",
+            "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'",
+            noremap = true,
+            silent = true,
+            expr = true
+        },
+
+        {
+            mode = "i",
+            "<C-j>",
+            "coc#pum#visible() ? coc#pum#next(1) : '<C-j>'",
+            noremap = true,
+            silent = true,
+            expr = true
+        },
+
+        {
+            mode = "i",
+            "<C-k>",
+            "coc#pum#visible() ? coc#pum#prev(1) : '<C-k>'",
+            noremap = true,
+            silent = true,
+            expr = true
+        },
+
+        {
+            mode = "i",
+            "<down>",
+            "coc#pum#visible() ? coc#pum#next(1) : '<down>'",
+            noremap = true,
+            silent = true,
+            expr = true
+        },
+
+        {
+            mode = "i",
+            "<up>",
+            "coc#pum#visible() ? coc#pum#prev(1) : '<up>'",
+            noremap = true,
+            silent = true,
+            expr = true
+        },
+
+        {
+            mode = "i",
+            "<CR>",
+            "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
+            silent = true,
+            expr = true,
+            noremap = true
+        },
+
+        { mode = "n", "<f7>", "<cmd>CocOutline<cr>" },
     }
 }
