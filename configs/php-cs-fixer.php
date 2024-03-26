70ce8b95589420a6107de79ea0328c46b9e6909e
Mohamad Supangat <hantamkoding@gmail.com>
2024-03-27 04:39:52 +0700

    cs fixer disable smfoni

--- configs/php-cs-fixer.php
+++ configs/php-cs-fixer.php
@@ -6,46 +6,48 @@ $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__);
 
 return (new PhpCsFixer\Config())
-    ->setRules([
-        'no_unused_imports'                                 => true,
-        '@Symfony'                                          => true,
-        '@Symfony:risky'                                    => true,
-        // 'declare_strict_types'                              => true,
-        // Prevent Psalm inline annotations being stripped. https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4446
-        // 'phpdoc_to_comment'                                => false,
-        // 'concat_space'                                     => ['spacing' => 'one'],
-        'fopen_flags'                                      => false,
-        'ordered_imports'                                  => true,
-        'protected_to_private'                             => false,
-        'binary_operator_spaces'                           => [
-            'operators' => [
-                '=>' => 'align',
-                '='  => 'align',
-                // Prevent spaces being added in PHP8 union types
-                '|'  => null,
+    ->setRules(
+        [
+            'no_unused_imports'                                 => true,
+            // '@Symfony'                                          => true,
+            // '@Symfony:risky'                                    => true,
+            // 'declare_strict_types'                              => true,
+            // Prevent Psalm inline annotations being stripped. https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4446
+            // 'phpdoc_to_comment'                                => false,
+            // 'concat_space'                                     => ['spacing' => 'one'],
+            'fopen_flags'                                      => false,
+            'ordered_imports'                                  => true,
+            'protected_to_private'                             => false,
+            'binary_operator_spaces'                           => [
+                'operators' => [
+                    '=>' => 'align',
+                    '='  => 'align',
+                    // Prevent spaces being added in PHP8 union types
+                    '|'  => null,
+                ],
             ],
+            // Bad yoda style is
+            'yoda_style'                    => false,
+            'phpdoc_annotation_without_dot' => false,
+            // 'cast_spaces'                   => ['space' => 'none'],
+            'no_superfluous_phpdoc_tags'    => [
+                'allow_mixed' => true,
+            ],
+            'echo_tag_syntax'               => false,
+            'global_namespace_import'       => [
+                'import_constants' => false,
+                'import_classes'   => false,
+            ],
+            'void_return'                                      => true,
+            'single_line_throw'                                => false,
+            'use_arrow_functions'                              => true,
+            'nullable_type_declaration_for_default_null_value' => true,
+            'trailing_comma_in_multiline'                      => [
+                'after_heredoc' => false,
+                'elements'      => ['arguments', 'arrays', 'match', 'parameters'],
+            ],
+            'ordered_interfaces' => true,
         ],
-        // Bad yoda style is
-        'yoda_style'                    => false,
-        'phpdoc_annotation_without_dot' => false,
-        'cast_spaces'                   => ['space' => 'none'],
-        'no_superfluous_phpdoc_tags'    => [
-            'allow_mixed' => true,
-        ],
-        'echo_tag_syntax'               => false,
-        'global_namespace_import'       => [
-            'import_constants' => false,
-            'import_classes'   => false,
-        ],
-        'void_return'                                      => true,
-        'single_line_throw'                                => false,
-        'use_arrow_functions'                              => true,
-        'nullable_type_declaration_for_default_null_value' => true,
-        'trailing_comma_in_multiline'                      => [
-            'after_heredoc' => false,
-            'elements'      => ['arguments', 'arrays', 'match', 'parameters'],
-        ],
-        'ordered_interfaces' => true,
-    ])
+    )
     ->setFinder($finder)
     ->setRiskyAllowed(true);
