8e000c24564d84f5f5f2c25acd77fcb506a11c85
Mohamad Supangat <hantamkoding@gmail.com>
2024-04-05 10:01:50 +0700

    add codesnap.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,4 +4,13 @@ vim.g.polyglot_disabled = variables.filetypes
 return {
     "sheerun/vim-polyglot",
     { "lewis6991/gitsigns.nvim", opts = {} },
+    {
+        "mistricky/codesnap.nvim",
+        build = "make",
+        opts = {
+            has_breadcrumbs = true,
+            watermark = nill,
+            mac_window_bar = false,
+        },
+    },
 }
