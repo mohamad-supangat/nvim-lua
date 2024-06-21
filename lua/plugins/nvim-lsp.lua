fd8570175d5af683e9a246fff188bc8b21ebe3b2
Mohamad Supangat <moha.supangat@gmail.com>
2024-06-21 22:48:51 +0700

    fix: revert

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -204,7 +204,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 -- ["<C-k>"] = cmp.mapping.select_prev_item(),
                 -- ["<C-j>"] = cmp.mapping.select_next_item(),
-                ["<C-k>"] = cmp.mapping(function(fallback)
+                ["<C-j>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.locally_jumpable(1) then
@@ -213,7 +213,7 @@ return {
                         fallback()
                     end
                 end, { "i", "s" }),
-                ["<C-j>"] = cmp.mapping(function(fallback)
+                ["<C-k>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                     elseif luasnip.locally_jumpable(-1) then
