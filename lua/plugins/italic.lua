708f7331dcbf627c8dd86894393bf849febe3f5c
Mohamad Supangat <moha.supangat@gmail.com>
2024-07-22 10:14:49 +0700

    italicize

---
+++ lua/plugins/italic.lua
@@ -0,0 +1,21 @@
+return {
+    "adityastomar67/italicize",
+    -- dev = true,
+    opts = {
+        transparency = false,
+        italics = true,
+        italics_groups = {
+            "Comment",
+            "Conditional",
+            "Identifier",
+            "SpecialChar",
+            "SpecialComment",
+            "String",
+            "Todo",
+            "CmpItemKind",
+            "NvimTreeGitRenamed",
+            "NvimTreeOpenedFile",
+            "NvimTreeRootFolder",
+        },
+    },
+}
