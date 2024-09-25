d1bd0a9206c885878a74bf9b5487f0d7b89df372
Mohamad Supangat <moha.supangat@gmail.com>
2024-09-25 22:22:12 +0700

    wow: paste image to neovim

---
+++ lua/plugins/img-clip.lua
@@ -0,0 +1,15 @@
+return {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+        default = {
+            prompt_for_file_name = false,
+            insert_mode_after_paste = false,
+            process_cmd = "convert - -resize 50% -quality 85 -",
+        },
+    },
+    keys = {
+        -- suggested keymap
+        { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+}
--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,11 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    enabled = false,
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
+    {
+        "iamcco/markdown-preview.nvim",
+        enabled = false,
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
