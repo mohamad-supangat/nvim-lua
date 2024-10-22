290367dec6e6dd50a91fe6cfa6d4ccbffb4008f4
Mohamad Supangat <moha.supangat@gmail.com>
2024-10-22 09:19:41 +0700

    new navic

---
+++ lua/plugins/navic.lua
@@ -0,0 +1,47 @@
+return {
+    "SmiteshP/nvim-navic",
+    opts = {
+        icons = {
+            File          = "󰈙 ",
+            Module        = " ",
+            Namespace     = "󰌗 ",
+            Package       = " ",
+            Class         = "󰌗 ",
+            Method        = "󰆧 ",
+            Property      = " ",
+            Field         = " ",
+            Constructor   = " ",
+            Enum          = "󰕘",
+            Interface     = "󰕘",
+            Function      = "󰊕 ",
+            Variable      = "󰆧 ",
+            Constant      = "󰏿 ",
+            String        = "󰀬 ",
+            Number        = "󰎠 ",
+            Boolean       = "◩ ",
+            Array         = "󰅪 ",
+            Object        = "󰅩 ",
+            Key           = "󰌋 ",
+            Null          = "󰟢 ",
+            EnumMember    = " ",
+            Struct        = "󰌗 ",
+            Event         = " ",
+            Operator      = "󰆕 ",
+            TypeParameter = "󰊄 ",
+        },
+        lsp = {
+            auto_attach = true,
+            preference = nil,
+        },
+        highlight = true,
+        separator = "  ",
+        depth_limit = 6,
+        depth_limit_indicator = "…",
+        safe_output = true,
+        lazy_update_context = false,
+        click = true,
+        format_text = function(text)
+            return text
+        end,
+    }
+}
