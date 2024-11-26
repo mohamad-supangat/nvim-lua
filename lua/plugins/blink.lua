23a5e8b26fcd5d1288dbfa3405c8fa86cf768f69
Mohamad Supangat <moha.supangat@gmail.com>
2024-11-26 12:04:28 +0700

    cmp and blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,28 +1,90 @@
-return {
+return
+{
     "saghen/blink.cmp",
     enabled = false,
-    event = { "LspAttach", "InsertCharPre" },
-    version = "v0.*",
+    -- event = { "LspAttach", "InsertCharPre" },
+    dependencies = {
+        {
+            "Exafunction/codeium.nvim",
+            enabled = true,
+            opts = {
+                enable_cmp_source = true
+                -- enable_chat = true,
+            },
+        },
+        {
+            "L3MON4D3/LuaSnip",
+            dev = false,
+            config = function()
+                require("luasnip.loaders.from_vscode").lazy_load({ paths = { "/home/deve/projects/snippets" } })
+                require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+        },
+        "saadparwaiz1/cmp_luasnip",
+        { 'saghen/blink.compat', opts = { impersonate_nvim_cmp = true } }
+    },
     opts = {
         highlight = {
             use_nvim_cmp_as_default = true,
         },
         nerd_font_variant = "normal",
-        accept = { auto_brackets = { enabled = true } },
+        accept = {
+            -- auto_brackets = { enabled = true },
+            expand_snippet = function(snippet) require('luasnip').lsp_expand(snippet) end,
+        },
 
-        -- trigger = { signature_help = { enabled = true } },
+        sources = {
+            completion = {
+                enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'codeium' },
+            },
+
+            providers = {
+                luasnip = {
+                    name = 'luasnip',
+                    module = 'blink.compat.source',
+                    score_offset = -3,
+                    opts = {
+                        use_show_condition = false,
+                        show_autosnippets = true,
+                    },
+                },
+                codeium = {
+                    name = "codeium",
+                    module = 'blink.compat.source',
+                    score_offset = -3,
+                }
+            },
+        },
+
 
         keymap = {
-            show = "<C-space>",
-            hide = { "<C-d>" },
-            accept = { "<CR>", "<Tab>" },
-            select_prev = { "<Up>", "<C-p>", "<C-k>" },
-            select_next = { "<Down>", "<C-n>", "<C-j>" },
-            show_documentation = {},
-            hide_documentation = {},
-            scroll_documentation_up = "<C-y>",
-            scroll_documentation_down = "<C-e>",
+            preset        = 'enter',
+            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            ['<C-e>']     = { 'hide', 'fallback' },
+            ['<S-Tab>']   = { 'snippet_backward', 'fallback' },
+
+            ['<Up>']      = { 'select_prev', 'fallback' },
+            ['<Down>']    = { 'select_next', 'fallback' },
+            ['<C-k>']     = { 'select_prev', 'fallback' },
+            ['<C-j>']     = { 'select_next', 'fallback' },
+
+            ['<C-b>']     = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>']     = { 'scroll_documentation_down', 'fallback' },
         },
+
+        -- trigger = { signature_help = { enabled = true } },
+        --
+        -- keymap = {
+        --     show = "<C-space>",
+        --     hide = { "<C-d>" },
+        --     accept = { "<CR>", "<Tab>" },
+        --     select_prev = { "<Up>", "<C-p>", "<C-k>" },
+        --     select_next = { "<Down>", "<C-n>", "<C-j>" },
+        --     show_documentation = {},
+        --     hide_documentation = {},
+        --     scroll_documentation_up = "<C-y>",
+        --     scroll_documentation_down = "<C-e>",
+        -- },
 
         windows = {
             autocomplete = {
@@ -33,4 +95,5 @@ return {
             },
         },
     },
+    opts_extend = { "sources.completion.enabled_providers" }
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,8 +5,11 @@ return {
     event = "InsertEnter",
     -- "hrsh7th/nvim-cmp",
     dependencies = {
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
+        {
+            "iguanacucumber/mag-buffer",
+            name = "cmp-buffer"
+        },
+        { "https://codeberg.org/FelipeLema/cmp-async-path.git" },
         {
             "L3MON4D3/LuaSnip",
             dev = false,
@@ -16,16 +19,9 @@ return {
             end,
         },
         "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lsp",
-        -- "hrsh7th/cmp-nvim-lua",
-        -- {
-        --     "hrsh7th/vim-vsnip",
-        --     init = function()
-        --         vim.g.vsnip_snippet_dir = "/home/deve/projects/snippets/snippets"
-        --     end,
-        -- },
-        -- "hrsh7th/cmp-vsnip",
-        -- "hrsh7th/cmp-cmdline",
+        { "iguanacucumber/mag-nvim-lsp",                       name = "cmp-nvim-lsp", opts = {} },
+        { "iguanacucumber/mag-cmdline",                        name = "cmp-cmdline" },
+        { "iguanacucumber/mag-nvim-lua",                       name = "cmp-nvim-lua" },
         {
             "Exafunction/codeium.nvim",
             enabled = true,
@@ -39,34 +35,52 @@ return {
     config = function()
         -- completion plugin setup
         local cmp = require("cmp")
-        -- cmp.setup.cmdline("/", {
-        --     mapping = cmp.mapping.preset.cmdline(),
-        --     sources = {
-        --         { name = "buffer" },
-        --     },
-        -- })
-        -- cmp.setup.cmdline(":", {
-        --     mapping = cmp.mapping.preset.cmdline(),
-        --     sources = cmp.config.sources({
-        --         { name = "path" },
-        --     }, {
-        --         {
-        --             name = "cmdline",
-        --             option = {
-        --                 ignore_cmds = { "Man", "!" },
-        --             },
-        --         },
-        --     }),
-        -- })
+
+        cmp.setup.cmdline("/", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "buffer" },
+            },
+        })
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" },
+            }, {
+                {
+                    name = "cmdline",
+                    option = {
+                        ignore_cmds = { "Man", "!" },
+                    },
+                },
+            }),
+        })
 
         local luasnip = require("luasnip")
+        local compare = require('cmp.config.compare')
+        local types = require('cmp.types')
         cmp.setup({
-            view = {
-                entries = "custom",
+            enabled = function()
+                local disabled = false
+                disabled = disabled or (vim.api.nvim_get_option_value('buftype', { buf = 0 }) == 'prompt')
+                disabled = disabled or (vim.fn.reg_recording() ~= '')
+                disabled = disabled or (vim.fn.reg_executing() ~= '')
+                return not disabled
+            end,
+
+            performance = {
+                debounce = 60,
+                throttle = 30,
+                fetching_timeout = 500,
+                filtering_context_budget = 3,
+                confirm_resolve_timeout = 80,
+                async_budget = 1,
+                max_view_entries = 200,
             },
+
+            preselect = types.cmp.PreselectMode.Item,
             snippet = {
                 expand = function(args)
-                    -- vim.fn["vsnip#anonymous"](args.body)
                     require("luasnip").lsp_expand(args.body)
                 end,
             },
@@ -119,21 +133,52 @@ return {
                 }),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
             }),
-            -- formatting = {
-            --     format = require("lspkind").cmp_format({
-            --         -- before = require("tailwind-tools.cmp").lspkind_format,
-            --         mode = "symbol_text",
-            --         maxwidth = 100,
-            --         ellipsis_char = "...",
-            --         symbol_map = { Codeium = "" },
-            --     }),
-            -- },
+
+            completion = {
+                autocomplete = {
+                    types.cmp.TriggerEvent.TextChanged,
+                },
+                completeopt = 'menu,menuone,noselect',
+                keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
+                keyword_length = 1,
+            },
+
+            formatting = {
+                expandable_indicator = true,
+                fields = { 'abbr', 'kind', 'menu' },
+                format = function(_, vim_item)
+                    return vim_item
+                end,
+            },
+
+            matching = {
+                disallow_fuzzy_matching = false,
+                disallow_fullfuzzy_matching = false,
+                disallow_partial_fuzzy_matching = true,
+                disallow_partial_matching = false,
+                disallow_prefix_unmatching = false,
+                disallow_symbol_nonprefix_matching = true,
+            },
+
+            sorting = {
+                priority_weight = 2,
+                comparators = {
+                    compare.offset,
+                    compare.exact,
+                    -- compare.scopes,
+                    compare.score,
+                    compare.recently_used,
+                    compare.locality,
+                    compare.kind,
+                    -- compare.sort_text,
+                    compare.length,
+                    compare.order,
+                },
+            },
             sources = {
                 { name = "codeium" },
                 { name = "luasnip" },
                 { name = "nvim_lsp" },
-                -- { name = "nvim_lua" },
-                -- { name = "vsnip" },
                 {
                     name = "buffer",
                     option = {
@@ -142,25 +187,20 @@ return {
                         end,
                     },
                 },
-                { name = "path" },
+                { name = 'async_path' }
+                -- { name = "path" },
             },
-            confirm_opts = { behavior = cmp.ConfirmBehavior.Replace, select = false },
+            confirmation = {
+                default_behavior = types.cmp.ConfirmBehavior.Insert,
+                get_commit_characters = function(commit_characters)
+                    return commit_characters
+                end,
+            },
+
             window = {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
-            sorting = {
-                comparators = {
-                    cmp.config.compare.score,
-                    cmp.config.compare.offset,
-                    cmp.config.compare.exact,
-                    cmp.config.compare.kind,
-                    cmp.config.compare.sort_text,
-                    cmp.config.compare.length,
-                    cmp.config.compare.order,
-                },
-            },
-            -- experimental = { ghost_text = false },
         })
     end,
 }
