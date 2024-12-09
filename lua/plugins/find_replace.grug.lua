715b8fa6b03bd1937fec23c8dc5d3acac03cdfbf
Mohamad Supangat <moha.supangat@gmail.com>
2024-12-09 08:45:13 +0700

    default find and replace is mutiline

--- lua/plugins/find_replace.grug.lua
+++ lua/plugins/find_replace.grug.lua
@@ -3,8 +3,10 @@ return {
     config = function()
         vim.g.maplocalleader = ","
         require("grug-far").setup({
-            -- options, see Configuration section below
-            -- there are no required options atm
+            prefills = {
+                search = "",
+                flags = "--multiline",
+            },
             -- engine = 'ripgrep' is default, but 'astgrep' can be specified
         })
     end,
