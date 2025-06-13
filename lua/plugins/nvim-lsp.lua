dd4e4d5ab73757a828f423f64998fc725e26511b
Mohamad Supangat <moha.supangat@gmail.com>
2025-06-13 08:24:05 +0700

    add vue_ls as rquired lsp

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -122,6 +122,8 @@ return {
       automatic_installation = false,
       ensure_installed = {
         "lua_ls",
+        "vue_ls",
+        "ts_ls",
         "emmet_language_server",
         "intelephense",
         -- "tailwindcss",
