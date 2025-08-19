c7191802e55144c5c2ad8dcc635a6bedff87c718
Mohamad Supangat <moha.supangat@gmail.com>
2025-08-19 15:15:35 +0700

    perbaiki fungsi copy path ke clipboard

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -85,7 +85,11 @@ vim.keymap.set("n", "<leader>bX", function()
 end, default_opts)
 
 -- copy current file path to clipboard
-vim.keymap.set("n", "<leader>cf", ":call setreg('*', @%, 'ac')<CR>", { desc = "Copy File Path to clipboard" })
+vim.keymap.set("n", "<leader>cf", function()
+  local filepath = vim.fn.expand("%")
+  vim.fn.setreg("+", filepath) -- write to clippoard
+  vim.notify("Copied filepath to clipboard")
+end, { desc = "Copy File Path to clipboard" })
 vim.keymap.set("n", "Y", "y$", default_opts) -- Change Y to copy to end of line and behave like C
 
 -- spliting window
