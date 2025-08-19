1db3e5da4d167ee68376919aab8199144a1a41e7
Mohamad Supangat <moha.supangat@gmail.com>
2025-08-19 10:07:33 +0700

    feat: theme

--- lua/plugins/theme.sonokai.lua
+++ lua/plugins/theme.sonokai.lua
@@ -1,7 +1,7 @@
 return {
   "sainnhe/sonokai",
   config = function()
-    -- vim.g.sonokai_style = "andromeda"
+    -- vim.g.sonokai_style = "atlantis"
     -- vim.g.sonokai_enable_italic = 1
     vim.g.sonokai_better_performance = 1
 
