eee00deca6e1103d46c4c03a30cd91c914bf57eb
Mohamad Supangat <moha.supangat@gmail.com>
2025-11-04 08:19:23 +0700

    enable notify using mini.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -72,14 +72,14 @@ return {
     }
 
     -- notify
-    -- local win_config = function()
-    --   local has_statusline = vim.o.laststatus > 0
-    --   local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
-    --   return { anchor = 'SE', col = vim.o.columns, row = vim.o.lines - pad }
-    -- end
-    -- require('mini.notify').setup({ window = { config = win_config } })
-    -- local opts = { ERROR = { duration = 10000 } }
-    -- vim.notify = require("mini.notify").make_notify({})
+    local win_config = function()
+      local has_statusline = vim.o.laststatus > 0
+      local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+      return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad }
+    end
+    require("mini.notify").setup({ window = { config = win_config } })
+    local opts = { ERROR = { duration = 10000 } }
+    vim.notify = require("mini.notify").make_notify({})
 
     -- mini sesssion
     require("mini.sessions").setup({
